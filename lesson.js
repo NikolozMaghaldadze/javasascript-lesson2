d070ab06505e74fc2ec3428bb60f76fc674fc030
NikaMaghaldadze <nikamaghaldadze13@gmail.com>
2022-10-20 22:14:38 +0400

    davaleba2

---
+++ lesson.js
@@ -0,0 +1,85 @@
+// davaleba 1
+
+// function sum(...ricxvebevi) {
+//     for( i = 0; i < ricxvebevi.length; i++) {
+//         if(ricxvebevi[i] > 0) {
+//             console.log(ricxvebevi[i]);
+//         }
+//     }
+// }
+// sum (2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+
+// davaleba 2
+
+// function jami(...numbers) {
+//     let numberjami = 0;
+//     for ( let item of numbers){
+//         numberjami = numberjami + item;
+//     }
+//     console.log(numberjami)
+// }
+// jami (10, 50, 6, 7, 8, 11, 6, 3, 9);
+
+
+// davaleba 3
+
+// let user = {
+//     firstName: 'giorgi',
+//     lastName: 'saakadze',
+//     age: 32,
+//     isloggedin: true
+//   };
+
+// let printName = ` ${user.firstName} ${user.lastName} ${user.isloggedin}`
+
+
+
+// console.log(printName);
+
+
+// davaleba 4
+
+// ვერ გავაკეთე :(
+
+// davaleba 5
+
+// let array = [1,2,4,10,34,5,7,87]
+
+// for (let z = 0; z < array.length; z++) {
+//     if (array[z]>0 && array[z]<10) {
+//         console.log(array[z]);
+//     }
+// }
+
+// davaleba 6
+
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] === 5) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+
+// davaleba 7
+
+// let array = ['html' , 'python' , 'css' , 'javascript' , 'bootstrap'];
+
+
+// for (let item of array) {
+//     if (item.length > 4 || item.includes('av')) {
+//         console.log(item);
+//     }
+// }
+
+// array. foreach(function(item){
+//     if (item.length > 4 || item.includes('av')) {
+//         console.log(item);
+//     }
+// })  ამ ვარიანტს ვერ გავუგე რაუნდა
+
+
+
+
+
